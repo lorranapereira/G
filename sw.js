1c28ce98934ffc78ca89cf897af7c4487fb93de8
lorranapereira <lorrana_pereira123@hotmail.com>
2018-04-15 20:17:50 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
 //This is the "Offline page" service worker
 
 //Install stage sets up the offline page in the cahche and opens a new cache
-self.addEventListener('install', function(event) {
-  var offlinePage = new Request('offline.html');
+this.addEventListener('install', function(event) {
   event.waitUntil(
-  fetch(offlinePage).then(function(response) {
-    return caches.open('pwabuilder-offline').then(function(cache) {
-      console.log('[PWA Builder] Cached offline page during Install'+ response.url);
-      return cache.put(offlinePage, response);
-    });
-  }));
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/sw-test/',
+        '/sw-test/index.html',
+        '/sw-test/css/style.css',
+    })
+  );
 });
 
 //If any fetch fails, it will show the offline page.
