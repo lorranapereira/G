3d5ca3a8045a9a037700b096a8d32d73452ff90e
lorranapereira <lorrana_pereira123@hotmail.com>
2018-04-16 00:04:23 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -14,6 +14,11 @@ var filesToCache = [
                     'imagens/icon3.png',
                     'imagens/icon4.jpg',
                     'imagens/fundo.jpg',
+                    'cadastro.html',
+                    'atendimento.html',
+                    'camisetas',
+                    'cadastro2.html'
+                    'cadastro2.html'
                     "/"
                  ];
 
