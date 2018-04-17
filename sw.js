f8280cdd5fa5288c41d3d655c67bd11a46422de3
lorranapereira <lorrana_pereira123@hotmail.com>
2018-04-17 14:32:40 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
-var CACHE_NAME = 'my-site-cache-v1';
+var CACHE_NAME = 'WELFORDIAN-CACHE-V2';
 var urlsToCache = [
-                    '/',
                     'index.html',
                     'css/style.css',
                     'imagens/logo.ico',
@@ -85,45 +84,39 @@ var urlsToCache = [
                     'imagens/shounen4.jpg',	
                     'imagens/shounen5.jpg',	
                     'imagens/shounen6.jpg',	
-                    'imagens/stranger.jpg'
+                    'imagens/stranger.jpg',
+                    '/'
 ];
 
 self.addEventListener('install', function(event) {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    .then(function(cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
   );
 });
+
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
+  var reqURL = new URL(event.request.url);
+  if (/lst.fm/.test(reqURL)) {
+    event.respondWith(lastFMImageResponse(event.request));
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
         if (response) {
           return response;
         }
 
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response.
         var fetchRequest = event.request.clone();
 
         return fetch(fetchRequest).then(
           function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
               return response;
             }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
             var responseToCache = response.clone();
 
             caches.open(CACHE_NAME)
@@ -134,27 +127,24 @@ self.addEventListener('fetch', function(event) {
             return response;
           }
         );
+      }).catch(function(err) {
+        return err;
       })
     );
+  }
 });
 
-self.addEventListener('activate', function(event) {
+function lastFMImageResponse(request) {
+  return caches.match(request).then(function(response) {
+    if (response) {
+      return response;
+    }
+    return fetch(request).then(function(response) {
+      caches.open('lfm-images').then(function(cache) {
+        cache.put(request, response);
+      });
 
-  var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
-
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
- 
-  
-
-     
+      return response.clone();
+    });
+  });
+}
